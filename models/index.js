5193fe4fd072db502d3682a3c1b78394688fa0d3
altavada <117209509+altavada@users.noreply.github.com>
2023-03-11 15:44:11 -0500

    Created models router

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'author_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'author_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'parent_id',
+});
+
+module.exports = {
+  User,
+  Post,
+  Comment,
+};
